34e44de421657efa890563f8a940eb6c9a28feab
Ilya_Kargapolov <ilya.kargapolov02@mail.ru>
2023-06-03 19:13:12 +0600

    Реализована страница Где нас найти

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -55,6 +55,23 @@
     </div>
 </nav>
 
+<div class="container">
+    <div class="row mt-5">
+        <div class="col">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2290.0513441654916!2d73.40020487614369!3d54.97219875184468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43aafde0c7512c3d%3A0x7d6bf4023be0dfe4!2z0JLQutGD0YHQvdGP0YjQutCwLCDQkdCw0L3QutC10YLQvdGL0Lkg0JfQsNC7IC0g0LfQsNC7INC90LUg0YHRg9GJ0LXRgdGC0LLRg9C10YI!5e0!3m2!1sru!2sru!4v1685797605524!5m2!1sru!2sru" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 22rem;">
+                <div class="card-body">
+                    <p class="card-text"><b>Адрес:</b> Омск, ул. Маяковского, дом 81</p>
+                    <p class="card-text"><b>Номер телефона:</b> +7 (123) 456-78-90</p>
+                    <p class="card-text"><b>Электронная почта:</b> vkusniashka@mail.ru</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <!--JQuery JS-->
 <script src="{{ asset('/js/jquery.js') }}"></script>
 <!--Bootstrap JS-->
